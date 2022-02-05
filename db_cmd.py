902e38ca055dee3acb6cc9fc31231a3595d46db1
icool5520 <icool5520@gmail.com>
2022-02-05 14:09:36 +0200

    func is admin

--- db_cmd.py
+++ db_cmd.py
@@ -24,7 +24,11 @@ def create_tables():
 						list_buy TEXT NOT NULL,
 						amount INTEGER NOT NULL,
 						active INTEGER NOT NULL)""")
-
+	with conn:
+		cur.execute(f"""CREATE TABLE IF NOT EXISTS admins(
+						user_id INTEGER PRIMARY KEY NOT NULL,
+						state TEXT NOT NULL,
+						data TEXT)""")
 def check_user_id(_user_id):
 	db_file = "db.db"
 	conn = None
@@ -64,6 +68,30 @@ def add_user(_user_id):
 	finally:
 		if conn is not None:
 			conn.close()
+
+
+def check_user_is_admin(_user_id):
+	db_file = "db.db"
+	conn = None
+	check = False
+	try:
+		sql = f"""SELECT * FROM admins WHERE user_id={_user_id}"""
+		conn = sqlite3.connect(db_file)
+		cur = conn.cursor()
+		cur.execute(sql)
+		data = cur.fetchone()
+		if data is not None:
+			cur.close()
+			check = True
+		else:
+			cur.close()
+	except Exception as ex:
+		print("check_user_is_admin", ex)
+	finally:
+		if conn is not None:
+			conn.close()
+		return check
+
 
 def get_categors():
 	db_file = "db.db"
